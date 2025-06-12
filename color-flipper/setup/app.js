4c04620e3b30d0c9a043c7629e10c98710aeceb9
rishabh2916 <rishabhrajpurohit1703@gmail.com>
2025-06-12 11:42:42 +0530

    simple color generator

--- color-flipper/setup/app.js
+++ color-flipper/setup/app.js
@@ -1,1 +1,16 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+  //get random NUmber between 0 - 3
+  const randomNUmber =  getRandomNumber();
+  console.log(randomNUmber)
+  document.body.style.backgroundColor = colors[randomNUmber];
+  color.textContent = colors[randomNUmber];
+})
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
